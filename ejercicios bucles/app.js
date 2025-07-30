89921930cf2de9c373e1e8e5c618f9a968ecdcf3
AlbertoReyes714 <ar7jars@gmail.com>
2025-07-30 01:25:49 -0600

    Add files via upload

---
+++ ejercicios bucles/app.js
@@ -0,0 +1,27 @@
+/*let contador = 1;
+while(contador <= 10 ){
+ console.log(contador);
+ contador++
+}
+ */
+/*
+let contador = 10;
+while(contador >= 0 ){
+    console.log(contador);
+ contador--;
+}
+ */
+/*
+let numero = prompt('Ingresa un número para iniciar la cuenta regresiva: ');
+while(numero >=0){
+ console.log(numero);
+ numero--;
+}
+ */
+
+let contador = 0;
+let numeroIncrementar = prompt('Ingresa un numero para iniciar a contar: ');
+while (contador <= numeroIncrementar){
+    console.log(contador);
+    contador++;
+}
